87276b8f37a082b719d6e25e45fcb0daddfb2736
Edward Iskandarov <edward@craftly.tech>
2020-12-11 23:18:20 +0500

    add initial config model

---
+++ detect_kit/config.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, HttpUrl, Field
+from typing import List, Optional
+
+
+class CertificateCheck(BaseModel):
+    site: HttpUrl
+    # Additional sites that certificate should work for
+    should_match_sites: Optional[List[HttpUrl]]
+    expiration_threshold_days: Optional[int] = Field(None, gt=0, le=365)
+    expiration_threshold_relative: Optional[int] = Field(None, gt=0, le=100)
+    expected_issuer_organisation_name: Optional[str]
+
+
+class DomainCheck(BaseModel):
+    domain: str
+    expiration_threshold_days: Optional[int] = Field(None, gt=0, le=365)
+    expiration_threshold_relative: Optional[int] = Field(None, gt=0, le=100)
+    expected_name_servers: Optional[str]
+    expected_registrar_name: Optional[str]
+
+
+class Settings(BaseModel):
+    version: int
+    certificates: List[CertificateCheck]
+    domains: List[DomainCheck]
