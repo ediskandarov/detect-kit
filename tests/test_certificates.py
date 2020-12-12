623e8cacf76400cfb721b261ff2a42c3c219dcbf
Edward Iskandarov <edward@craftly.tech>
2020-12-12 21:51:04 +0500

    add more certificate tests

--- tests/test_certificates.py
+++ tests/test_certificates.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -8,45 +8,87 @@ from tests.types import CertificateFactory
 pytestmark = pytest.mark.detection
 
 
-def test_certificate_is_not_expired(
+def test_certificate_is_active(
     cert_cfg: CertificateCheck, get_certificate: CertificateFactory
-):
+) -> None:
     cert = get_certificate(cert_cfg)
 
     now = datetime.utcnow()
-    if cert_cfg.expiration_threshold_days is not None:
-        if cert.not_before is not None:
-            assert (
-                now > cert.not_before
-            ), f"Certificate not valid before {cert.not_before}"
-        if cert.not_after is not None:
-            assert (
-                now < cert.not_after
-            ), f"Certificate is not valid after {cert.not_after}"
+    if cert.not_before is not None:
+        assert now > cert.not_before, f"Certificate not valid before {cert.not_before}"
+    if cert.not_after is not None:
+        assert now < cert.not_after, f"Certificate is not valid after {cert.not_after}"
+
+
+def test_certificate_expiration_check(
+    cert_cfg: CertificateCheck, get_certificate: CertificateFactory
+) -> None:
+    cert = get_certificate(cert_cfg)
+
+    if cert_cfg.expiration_threshold_days is None or cert.not_after is None:
+        return
+
+    now = datetime.utcnow()
+    threshold = timedelta(days=cert_cfg.expiration_threshold_days)
+    notify_on = cert.not_after - threshold
+    assert (
+        now < notify_on
+    ), f"Warning! Certificate will expire in less than {threshold}"
+
+
+def test_certificate_expiration_check_relative(
+    cert_cfg: CertificateCheck, get_certificate: CertificateFactory
+) -> None:
+    cert = get_certificate(cert_cfg)
+
+    if (
+        cert_cfg.expiration_threshold_relative is None
+        or cert.not_after is None
+        or cert.not_before is None
+    ):
+        return
+
+    now = datetime.utcnow()
+    certificate_valid_perior = cert.not_after - cert.not_before
+    threshold = certificate_valid_perior * cert_cfg.expiration_threshold_relative / 100
+    notify_on = cert.not_after - threshold
+    assert (
+        now < notify_on
+    ), f"Warning! Certificate will expire in less than {threshold}"
 
 
 def test_certificate_issuer(
     cert_cfg: CertificateCheck, get_certificate: CertificateFactory
-):
+) -> None:
     cert = get_certificate(cert_cfg)
 
-    if cert_cfg.expected_issuer_organisation_name is not None:
-        if cert.issuer.organization_name is not None:
-            assert (
-                cert_cfg.expected_issuer_organisation_name.lower()
-                == cert.issuer.organization_name.lower()
-            )
+    if (
+        cert_cfg.expected_issuer_organisation_name is None
+        or cert.issuer.organization_name is None
+    ):
+        return
+
+    assert (
+        cert_cfg.expected_issuer_organisation_name.lower()
+        == cert.issuer.organization_name.lower()
+    )
 
 
 def test_certificate_match_sites(
     cert_cfg: CertificateCheck, get_certificate: CertificateFactory
-):
+) -> None:
     cert = get_certificate(cert_cfg)
 
     assert cert.match_site(
         cert_cfg.site
     ), f"Certificate does not match for site {cert_cfg.site}"
 
-    if cert_cfg.should_match_sites is not None:
-        for site in cert_cfg.should_match_sites:
-            assert cert.match_site(site), f"Certificate does not match for site {site}"
+    if cert_cfg.should_match_sites is None:
+        return
+
+    for site in cert_cfg.should_match_sites:
+        assert cert.match_site(site), f"Certificate does not match for site {site}"
+
+
+def test_certificate_is_trusted(cert_cfg):
+    assert False
