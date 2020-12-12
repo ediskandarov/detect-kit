106f4a82ac9cd82693f1da23bec0d3db09a32762
Edward Iskandarov <edward@craftly.tech>
2020-12-12 15:08:33 +0500

    settings model parses config file

--- detect_kit/config.py
+++ detect_kit/config.py
@@ -1,5 +1,9 @@
-from typing import Optional, Sequence
+from __future__ import annotations
 
+from pathlib import Path
+from typing import Optional, Sequence, Union
+
+import yaml
 from pydantic import BaseModel, Field, HttpUrl
 
 
@@ -16,11 +20,24 @@ class DomainCheck(BaseModel):
     domain: str
     expiration_threshold_days: Optional[int] = Field(None, gt=0, le=365)
     expiration_threshold_relative: Optional[int] = Field(None, gt=0, le=100)
-    expected_name_servers: Optional[str]
+    expected_name_servers: Optional[Sequence[str]]
     expected_registrar_name: Optional[str]
 
 
-class Settings(BaseModel):
-    version: int
+class DetectKitSettings(BaseModel):
     certificates: Optional[Sequence[CertificateCheck]]
     domains: Optional[Sequence[DomainCheck]]
+
+
+class Settings(BaseModel):
+    version: str
+    detect_kit: DetectKitSettings
+
+    @classmethod
+    def from_config_file(cls, config_path: Union[Path, str]) -> Settings:
+        with open(config_path) as f:
+            raw_config = yaml.safe_load(f)
+
+        settings = cls.parse_obj(raw_config)
+
+        return settings
